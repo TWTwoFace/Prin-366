c735147a1af42e481dfed79e089a1a663d45d160
Alexander Popov <pipo2017@mail.ru>
2026-03-16 12:36:44 +0300

    feat: add login field to user registration schema

--- PopovAlexander.py
+++ PopovAlexander.py
@@ -17,3 +17,5 @@ class UserLoginSchema(BaseModel):
 
 class UserRegisterSchema(UserLoginSchema):
     nickname: str = Field(min_length=5, max_length=20)
+    login: str = Field(min_length=5, max_length=20)
+    
